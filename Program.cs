17005964b0dd3f8036f300e2e2fc836679a91a70
AlpDereli <alpdereli26@gmail.com>
2023-08-22 20:13:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+SphereMaker make = new SphereMaker();
+int r = make.radiustake();
+make.maker(r);
+
+class SphereMaker
+{
+    public int radiustake()
+    {
+        Console.WriteLine("Please enter the radius of the sphere as an integer: ");
+        int r = int.Parse(Console.ReadLine());
+        return r;
+    }
+    public void maker(int radius)
+    {
+        for (int y = (int)-radius; y <= radius; y++)
+        {
+            for (int x = (int)-radius; x <= radius; x++)
+            {
+                double distance = Math.Sqrt(x * x + y * y);
+                if (distance <= radius)
+                {
+                    Console.Write("* ");
+                }
+                else
+                {
+                    Console.Write("  ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
